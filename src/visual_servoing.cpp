d747b1f5969fe6dd890b177520026d69f2643d8b
jpereira <joaomacpereira@tecnico.ulisboa.pt>
2020-08-18 11:43:09 +0100

    Make visual servoing start by ROS service call

--- src/visual_servoing.cpp
+++ src/visual_servoing.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <std_srvs/Trigger.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Twist.h>
 #include <tf/transform_datatypes.h>
@@ -20,75 +21,90 @@ tf::Transform errorTf;
 
 double visual_servoing_k, visual_servoing_speed_cap;
 
-void visual_servo(const ros::TimerEvent&) {
-  ROS_INFO("----------");
+ros::Rate vs_rate(20); // 20Hz
 
-  try{
-    eefMarkerVisionTransform = tfBuffer.lookupTransform("root", "grasp_tool", ros::Time(0), ros::Duration(5.0));
-    targetTransform = tfBuffer.lookupTransform("root", target_frame, ros::Time(0), ros::Duration(5.0));
+bool visual_servo(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
+  while(true) {
+    ROS_INFO("----------");
+
+    try{
+      eefMarkerVisionTransform = tfBuffer.lookupTransform("root", "grasp_tool", ros::Time(0), ros::Duration(5.0));
+      targetTransform = tfBuffer.lookupTransform("root", target_frame, ros::Time(0), ros::Duration(5.0));
+    }
+    catch (tf2::TransformException &ex) {
+      ROS_ERROR("Error getting (eef -> target) transform: %s",ex.what());
+      res.success = false;
+      res.message = "Error getting (eef -> target) transform";
+      return true;
+    }
+    ROS_INFO("eefMarkerVisionTransform: X %f | Y %f | Z %f", eefMarkerVisionTransform.transform.translation.x, eefMarkerVisionTransform.transform.translation.y, eefMarkerVisionTransform.transform.translation.z);
+    ROS_INFO("targetTransform: X %f | Y %f | Z %f", targetTransform.transform.translation.x, targetTransform.transform.translation.y, targetTransform.transform.translation.z);
+
+
+    tf::transformStampedMsgToTF(eefMarkerVisionTransform, vTf); // vision
+    tf::transformStampedMsgToTF(targetTransform, targetTf);
+
+    ROS_INFO("vTf: X %f | Y %f | Z %f", vTf.getOrigin().getX(), vTf.getOrigin().getY(), vTf.getOrigin().getZ());
+    ROS_INFO("targetTf: X %f | Y %f | Z %f", targetTf.getOrigin().getX(), targetTf.getOrigin().getY(), targetTf.getOrigin().getZ());
+
+    // Discarding rotation, because we only care about translation
+    //vTf.setRotation(tf::Quaternion(0, 0, 0, 1));
+    //mTf.setRotation(tf::Quaternion(0, 0, 0, 1));
+    targetTf.setRotation(tf::Quaternion(0, 0, 0, 1));
+
+    // multiply first, then discard rotation
+    //vTf = mTf*vTf;
+    vTf.setRotation(tf::Quaternion(0, 0, 0, 1));
+
+    //errorTf = (vTf*mTf).inverseTimes(targetTf);
+    errorTf = vTf.inverseTimes(targetTf);
+
+    ROS_INFO("errorTf: X %f | Y %f | Z %f", errorTf.getOrigin().getX(), errorTf.getOrigin().getY(), errorTf.getOrigin().getZ());
+
+    tf::transformTFToMsg(errorTf, errorTransform);
+
+    ROS_INFO("errorTransform: X %f | Y %f | Z %f", errorTransform.translation.x, errorTransform.translation.y, errorTransform.translation.z);
+
+    ROS_INFO("----------");
+
+    double magnitude = sqrt( pow(errorTransform.translation.x, 2.0) + pow(errorTransform.translation.y, 2.0) + pow(errorTransform.translation.z, 2.0) );
+    if(magnitude < 0.1) {
+      // Converged
+      ROS_INFO("Visual servoing finished: reached desired distance to goal.");
+      res.success = true;
+      return true;
+    }
+
+    // Scale the transform vector, based on K
+    errorTransform.translation.x *= visual_servoing_k;
+    errorTransform.translation.y *= visual_servoing_k;
+    errorTransform.translation.z *= visual_servoing_k;
+
+    // clamp the vector's magnitude (speed cap)
+    magnitude = sqrt( pow(errorTransform.translation.x, 2.0) + pow(errorTransform.translation.y, 2.0) + pow(errorTransform.translation.z, 2.0) );
+    if(magnitude > visual_servoing_speed_cap) { 
+      // Divide transform by its magnitude : normalize it to length 1
+      errorTransform.translation.x /= magnitude;
+      errorTransform.translation.y /= magnitude;
+      errorTransform.translation.z /= magnitude;
+
+      // Multiply by speed cap
+      errorTransform.translation.x *= visual_servoing_speed_cap;
+      errorTransform.translation.y *= visual_servoing_speed_cap;
+      errorTransform.translation.z *= visual_servoing_speed_cap;
+    }
+
+    ROS_INFO("Sending x: %f,y: %f,z: %f", errorTransform.translation.x, errorTransform.translation.y, errorTransform.translation.z);
+
+    geometry_msgs::Twist twist;
+    twist.linear.x = errorTransform.translation.x;
+    twist.linear.y = errorTransform.translation.y;
+    twist.linear.z = errorTransform.translation.z;
+
+    vel_pub.publish(twist);
+
+    vs_rate.sleep();
   }
-  catch (tf2::TransformException &ex) {
-    ROS_ERROR("Error getting (eef -> target) transform: %s",ex.what());
-    ros::shutdown();
-    return;
-  }
-  ROS_INFO("eefMarkerVisionTransform: X %f | Y %f | Z %f", eefMarkerVisionTransform.transform.translation.x, eefMarkerVisionTransform.transform.translation.y, eefMarkerVisionTransform.transform.translation.z);
-  ROS_INFO("targetTransform: X %f | Y %f | Z %f", targetTransform.transform.translation.x, targetTransform.transform.translation.y, targetTransform.transform.translation.z);
-
-
-  tf::transformStampedMsgToTF(eefMarkerVisionTransform, vTf); // vision
-  tf::transformStampedMsgToTF(targetTransform, targetTf);
-
-  ROS_INFO("vTf: X %f | Y %f | Z %f", vTf.getOrigin().getX(), vTf.getOrigin().getY(), vTf.getOrigin().getZ());
-  ROS_INFO("targetTf: X %f | Y %f | Z %f", targetTf.getOrigin().getX(), targetTf.getOrigin().getY(), targetTf.getOrigin().getZ());
-
-  // Discarding rotation, because we only care about translation
-  //vTf.setRotation(tf::Quaternion(0, 0, 0, 1));
-  //mTf.setRotation(tf::Quaternion(0, 0, 0, 1));
-  targetTf.setRotation(tf::Quaternion(0, 0, 0, 1));
-
-  // multiply first, then discard rotation
-  //vTf = mTf*vTf;
-  vTf.setRotation(tf::Quaternion(0, 0, 0, 1));
-
-  //errorTf = (vTf*mTf).inverseTimes(targetTf);
-  errorTf = vTf.inverseTimes(targetTf);
-
-  ROS_INFO("errorTf: X %f | Y %f | Z %f", errorTf.getOrigin().getX(), errorTf.getOrigin().getY(), errorTf.getOrigin().getZ());
-
-  tf::transformTFToMsg(errorTf, errorTransform);
-
-  ROS_INFO("errorTransform: X %f | Y %f | Z %f", errorTransform.translation.x, errorTransform.translation.y, errorTransform.translation.z);
-
-  ROS_INFO("----------");
-
-  // Scale the transform vector, based on K
-  errorTransform.translation.x *= visual_servoing_k;
-  errorTransform.translation.y *= visual_servoing_k;
-  errorTransform.translation.z *= visual_servoing_k;
-
-  // clamp the vector's magnitude (speed cap)
-  double magnitude = sqrt( pow(targetTransform.transform.translation.x, 2.0) + pow(targetTransform.transform.translation.y, 2.0) + pow(targetTransform.transform.translation.z, 2.0) );
-  if(magnitude > visual_servoing_speed_cap) { 
-    // Divide transform by its magnitude : normalize it to length 1
-    errorTransform.translation.x /= magnitude;
-    errorTransform.translation.y /= magnitude;
-    errorTransform.translation.z /= magnitude;
-
-    // Multiply by speed cap
-    errorTransform.translation.x *= visual_servoing_speed_cap;
-    errorTransform.translation.y *= visual_servoing_speed_cap;
-    errorTransform.translation.z *= visual_servoing_speed_cap;
-  }
-
-  ROS_INFO("Sending x: %f,y: %f,z: %f", errorTransform.translation.x, errorTransform.translation.y, errorTransform.translation.z);
-
-  geometry_msgs::Twist twist;
-  twist.linear.x = errorTransform.translation.x;
-  twist.linear.y = errorTransform.translation.y;
-  twist.linear.z = errorTransform.translation.z;
-
-  vel_pub.publish(twist);
 }
 
 int main(int argc, char** argv) {
@@ -113,7 +129,7 @@ int main(int argc, char** argv) {
     ROS_ERROR("'visual_servoing_k' param not given");
     ros::shutdown();
   }
-  ROS_INFO("Visual-servoing K: %d", visual_servoing_k);
+  ROS_INFO("Visual-servoing K: %f", visual_servoing_k);
 
   if(node_handle.hasParam("visual_servoing_speed_cap")) {
     node_handle.getParam("visual_servoing_speed_cap", visual_servoing_speed_cap);
@@ -121,15 +137,14 @@ int main(int argc, char** argv) {
     ROS_ERROR("'visual_servoing_speed_cap' param not given");
     ros::shutdown();
   }
-  ROS_INFO("Visual-servoing speed cap: %d", visual_servoing_speed_cap);
+  ROS_INFO("Visual-servoing speed cap: %f", visual_servoing_speed_cap);
 
   tf2_ros::TransformListener tfListener(tfBuffer);
 
   ros::Duration(2).sleep();
 
-  ros::Timer timer = node_handle.createTimer(ros::Duration(0.2), visual_servo); //5Hz
+  ros::ServiceServer vs_server = node_handle.advertiseService("/kinova_manipulation/visual_servo", visual_servo);
 
   ros::waitForShutdown();
-
   return 0;
 }
